624d7c2dc200cb04c179dd2fbcb91e04f3b46b45
Valluri Venkatram <vallurivenkatram245913@gmail.com>
2025-03-30 18:18:41 +0530

    changed footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -22,7 +22,7 @@ export const Footer = () => {
       <div className="absolute bottom-0 left-1/2 -z-10 h-[400px] w-[1600px] -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]"></div>
       <div className="container">
         <div className="flex flex-col items-center gap-8 border-t border-white/15 py-6 text-sm md:flex-row md:justify-between">
-          <div className="text-white/40">&copy; 2025. All rights reserved.</div>
+          <div className="text-white/40">&copy; Valluri Venkatram 2025. All rights reserved.</div>
           <nav className="flex flex-col items-center gap-8 md:flex-row">
             {footerLinks.map((link) => (
               <Link
