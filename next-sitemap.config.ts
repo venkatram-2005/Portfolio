4550525d3ae46ce6d30f1e98c6e337daff766736
Valluri Venkatram <vallurivenkatram245913@gmail.com>
2025-03-31 23:10:21 +0530

    sitemap

---
+++ next-sitemap.config.ts
@@ -0,0 +1,8 @@
+import { IConfig } from 'next-sitemap';
+
+const config: IConfig = {
+  siteUrl: 'https://venkatram.vercel.app',
+  generateRobotsTxt: true, // Generates robots.txt along with the sitemap
+};
+
+export default config;
