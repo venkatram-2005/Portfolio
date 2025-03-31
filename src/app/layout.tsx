c7dfbc8ddbd9e30ab1ab0983cbf27443585d5e82
Valluri Venkatram <vallurivenkatram245913@gmail.com>
2025-03-31 22:40:56 +0530

    added meta for verf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="74G8nRsYV1eK2nl0oqIzCxDLPhzDE2GrUC-JUBQc0qg" />
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body
